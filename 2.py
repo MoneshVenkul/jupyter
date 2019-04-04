be9ff7fea7216383a4d663221eded92b04508182
MoneshVenkul <48666307+MoneshVenkul@users.noreply.github.com>
2019-04-04 20:24:21 +0530

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,22 @@
+course = "machine learning"
+for c in course:
+    print(c)
+
+courses = ['python', 'iot', 'uit']
+for c in courses:
+    print(c)
+
+for i in range(6):
+    print(i)
+
+for i in range(1, 10):
+    print(i)
+
+for i in range(1, 10, 2):
+    print(i)
+
+courses = ['python', 'iot', 'uit']
+staff = ['x', 'y', 'z']
+for i in staff:
+    for j in courses:
+        print(i,j)
