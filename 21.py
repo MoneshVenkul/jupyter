7d1b44024a188e7390b0958c516225ffd4759798
MoneshVenkul <48666307+MoneshVenkul@users.noreply.github.com>
2019-04-04 16:14:30 +0530

    Update and rename range.py to 21.py

--- 21.py
+++ 21.py
@@ -1,10 +1,10 @@
 def evendigit(l,u):
     items =  []
-    for i in range(2000,3000):
+    for i in range(l,u):
         s =str(i)
         if(int(s[0])%2==0)and(int(s[1])%2==0)and (int(s[2])%2==0) and (int(s[3])%2==0):
             items.append(s)
         print(",".join(items))
-l=input('enter     the     lower     limit')
-u=input('enter     the     upper     limit')
+l=int(input('enter     the     lower     limit'))
+u=int(input('enter     the     upper     limit'))
 evendigit(l,u)
