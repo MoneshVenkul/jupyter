3da08a0c257eca84e375ee5b4c6ef777a0e120f4
MoneshVenkul <48666307+MoneshVenkul@users.noreply.github.com>
2019-04-04 19:23:20 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,11 @@
+import exe4 as cal
+def add(a,b):
+    return a+b
+num1=input("Enter number 1:")
+num2=input("Enter number 2:")
+result=add(int(num1),int(num2))
+print(result)
+result=cal.sub(int(num1),int(num2))
+print(result)
+
+#create an py file for exe4
